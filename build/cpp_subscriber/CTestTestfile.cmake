fc396e619a44a8a409428945bda707cb90d78954
JackieLonger <611221404@gms.ndhu.edu.tw>
2024-09-24 17:56:08 +0800

    First Commit

---
+++ build/cpp_subscriber/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jackiepi4/test_ws/src/cpp_subscriber
+# Build directory: /home/jackiepi4/test_ws/build/cpp_subscriber
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
